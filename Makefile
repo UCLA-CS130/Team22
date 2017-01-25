385b1b18cdcaf57dc3d5d991580ec22ae562e5fb
David Stephan <stephandavid3@gmail.com>
2017-01-24 18:15:39 -0800

    main uses GetPort

--- Makefile
+++ Makefile
@@ -1,14 +1,20 @@
 CXX=g++
 CXXFLAGS=-std=c++11 -I. -Wall -Werror
 BOOSTFLAGS = -lboost_system
-DEPS=server.h connection.h
-OBJ=server.o connection.o main.o
+DEPS=server.h connection.h config_parser.h
+OBJ=server.o connection.o config_parser.o main.o
 
 %.o: %.c $(DEPS)
 	$(CC) $(CXXFLAGS) -c -o $@ $<
 
 webserver: $(OBJ)
 	g++ $(CXXFLAGS) -o $@ $^ $(BOOSTFLAGS)
-
+	
+test:
+	./build_tests.sh
+	./config_parser_test
+	
+test-curl:
+	curl http://localhost:4000
 clean:
 	rm -f $(OBJ) webserver
--- main.cpp
+++ main.cpp
@@ -9,21 +9,32 @@ int main(int argc, char* argv[])
 	{
 		if (argc != 2)
 		{
-			std::cerr << "Usage: web_server <port>\n";
+			std::cerr << "Usage: web_server <config file>\n";
+			return 1;
+		}
+
+		NginxConfigParser parser;
+		NginxConfig out_config;
+		int port;
+		if (!parser.Parse(argv[1], &out_config)) {
+			return 1;
+		}
+		port = out_config.GetPort();
+		if (port == -1) {
+			std::cerr << "Error reading port from config file. Expecting: \"port [portnumber];\" in config file.";
 			return 1;
 		}
 
 		boost::asio::io_service io_service;
 
-    using namespace std; // For atoi.
-    Server s(io_service, atoi(argv[1]));
+		Server s(io_service, port);
 
-    io_service.run();
-  }
-  catch (std::exception& e)
-  {
-  	std::cerr << "Exception: " << e.what() << "\n";
-  }
+		io_service.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
 
-  return 0;
+	return 0;
 }
