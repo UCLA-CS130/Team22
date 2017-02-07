cba4946af3d0c9e38521c04dfabd8aa2b53841b2
Kevin Zhu <kevinazhu@gmail.com>
2017-02-07 00:03:28 -0800

    add 404 test to file handler

--- file_handler_test.cc
+++ file_handler_test.cc
@@ -16,3 +16,16 @@ TEST(FileHandlerTest, SimpleTest) {
 	std::string response = fileHandler.GenerateResponse(*headerInfo, request);
 	EXPECT_TRUE(response.length() > 200);
 }
+
+TEST(FileHandlerTest, Simple404Test) {
+	std::string request = "GET /static/error.jpg HTTP/1.1\r\n\r\n";
+
+	// generate header info from the request
+	auto headerInfo = HttpParser::MakeHttpParser(request.c_str());
+
+	// create an echo handler
+	FileHandler fileHandler("static/");
+
+	std::string response = fileHandler.GenerateResponse(*headerInfo, request);
+	EXPECT_FALSE(response.length() > 200);
+}
