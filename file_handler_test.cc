998f6957216b54d7024dad7c8d5e90cb91a1b319
Kevin Zhu <kevinazhu@gmail.com>
2017-02-06 23:56:18 -0800

    add file_handler_test file

---
+++ file_handler_test.cc
@@ -0,0 +1,18 @@
+#include <string>
+
+#include "gtest/gtest.h"
+#include "file_handler.h"
+#include "http_parser.h"
+
+TEST(FileHandlerTest, SimpleTest) {
+	std::string request = "GET /static/kinkakuji.jpg HTTP/1.1\r\n\r\n";
+
+	// generate header info from the request
+	auto headerInfo = HttpParser::MakeHttpParser(request.c_str());
+
+	// create an echo handler
+	FileHandler fileHandler("static/");
+
+	std::string response = fileHandler.GenerateResponse(*headerInfo, request);
+	EXPECT_TRUE(response.length() > 200);
+}
