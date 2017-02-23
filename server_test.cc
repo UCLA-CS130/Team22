1111149b8c96d3293452eb2f6b391e8199203803
Justin <jteo1@yahoo.com>
2017-02-22 21:27:52 -0800

    clean up some tabs/space mismatches

--- server_test.cc
+++ server_test.cc
@@ -8,38 +8,38 @@
 //Test various server configs
 class MakeServerTest : public ::testing::Test {
 protected:
-  Server* parseConfigString(const std::string config_string) {
-    std::stringstream config_stream(config_string);
-    if(!parser.Parse(&config_stream, &out_config)) {
-    	return nullptr;
+    Server* parseConfigString(const std::string config_string) {
+        std::stringstream config_stream(config_string);
+        if(!parser.Parse(&config_stream, &out_config)) {
+            return nullptr;
+        }
+
+        Server* server = Server::MakeServer(io_service, out_config);
+        return server;
     }
 
-		Server* server = Server::MakeServer(io_service, out_config);
-		return server;
-  }
 
-
-  boost::asio::io_service io_service;
-	NginxConfigParser parser;
-	NginxConfig out_config;
+    boost::asio::io_service io_service;
+    NginxConfigParser parser;
+    NginxConfig out_config;
 };
 
 //Test for valid config
 TEST_F(MakeServerTest, ValidConfig) {
     Server* s = parseConfigString("port 8080;");
-	EXPECT_TRUE(s);
+    EXPECT_TRUE(s);
     delete s;
 }
 
 // Test for invalid port
 TEST_F(MakeServerTest, InvalidStringPortConfig) {
-	EXPECT_FALSE(parseConfigString("port hello;"));
+    EXPECT_FALSE(parseConfigString("port hello;"));
     EXPECT_FALSE(parseConfigString("port 68000;"));
 }
 
 // Test for missing port definition
 TEST_F(MakeServerTest, MissingPortConfig) {
-	EXPECT_FALSE(parseConfigString(""));
+    EXPECT_FALSE(parseConfigString(""));
 }
 //test handler population is working
 TEST_F(MakeServerTest, ValidHandlers) {
