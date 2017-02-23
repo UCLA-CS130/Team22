ab8cfbfe9ad480fffacfa33a306f52f55a356add
jteo1 <jteo1@yahoo.com>
2017-02-22 16:17:38 -0800

    remove old cout

--- request_handler.cc
+++ request_handler.cc
@@ -12,7 +12,6 @@ std::map<std::string, RequestHandler* (*)(void)>* request_handler_builders = new
 RequestHandler* RequestHandler::CreateByName(const std::string& type) {
 	const auto type_and_builder = request_handler_builders->find(type);
 	if (type_and_builder == request_handler_builders->end()) {
-		std::cout << "TEST1" << std::endl;
 		return nullptr;
 	}
 	return (*type_and_builder->second)();
