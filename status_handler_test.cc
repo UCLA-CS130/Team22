cfb499198921d5f5db03e9c47b9bbd9e6aa0bd1f
David Stephan <stephandavid3@gmail.com>
2017-02-22 22:42:48 -0800

    more comprehensive test for status handler

--- status_handler_test.cc
+++ status_handler_test.cc
@@ -18,7 +18,14 @@ protected:
 		StatusHandler* shandler = dynamic_cast<StatusHandler*>(handler);
 		statusHandler = std::unique_ptr<StatusHandler>(shandler);
 	}
+	RequestHandler::Status InitString(std::string uri, std::string config) {
+		std::stringstream ss(config);
+		parser.Parse(&ss, &out_config);
+		return statusHandler->Init(uri, out_config);
+	}
 
+	NginxConfigParser parser;
+	NginxConfig out_config;
 	std::unique_ptr<StatusHandler> statusHandler;
 };
 
@@ -47,4 +54,34 @@ TEST(StatusHandlerTest, SimpleTest) {
 TEST_F(StatusHandlerFixture, StatusHandlerConstruction) {
 	// can we even get this far?
 	ASSERT_NE(statusHandler.get(), nullptr);
+
+	// Init shouldn't require anything
+	ASSERT_EQ(InitString("",""), RequestHandler::OK);
+
+	// Special init
+	ServerStatus status;
+	statusHandler->InitStatusHandler(&status);
+
+	// prepare the ServerStatus
+	status.LogRequest("/some/url", 5555);
+	status.LogRequest("/some/url", 2222);
+	status.LogRequest("/some/url", 1111);
+	status.LogRequest("/someother/url2", 9999);
+
+	// Handle a request
+	auto request = Request::Parse("GET /echo HTTP/1.1\r\n\r\n");
+	Response response;
+	statusHandler->HandleRequest(*request, &response);
+
+	std::string result = response.ToString();
+
+	// some general expectations for the html result
+	EXPECT_GT(result.length(), 150);
+	EXPECT_NE(result.find("5555"), std::string::npos);
+	EXPECT_NE(result.find("9999"), std::string::npos);
+	EXPECT_NE(result.find("/some/url"), std::string::npos);
+	EXPECT_NE(result.find("4"), std::string::npos); // total number of requests
+	EXPECT_NE(result.find("3"), std::string::npos); // count of /some/url
+
+	//std::cout << result;
 }
