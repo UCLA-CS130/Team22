f3ef50f3666aea4558e3dc2069627019da263d38
Justin <jteo1@yahoo.com>
2017-02-22 18:43:51 -0800

    allow users to add -d flag to allow verbose debug printing

--- main.cc
+++ main.cc
@@ -3,17 +3,31 @@
 #include "server.h"
 #include "config_parser.h"
 #include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
 
 int main(int argc, char* argv[])
 {
 	BOOST_LOG_TRIVIAL(info) << "Starting up the server...";
 	try
 	{
-		if (argc != 2)
+		//expect command to be './webserver config' or './webserver config -d'
+		if (argc > 3 || argc < 2)
 		{
-			BOOST_LOG_TRIVIAL(fatal) << "Incorrect command. Usage: web_server <config file>.";
+			BOOST_LOG_TRIVIAL(fatal) << "Incorrect command. Usage: web_server <config file>. Add \'-d\' to the end to enable all debugging trace logs.";
 			return 1;
 		}
+
+		//logging level is everything by default
+		if(argc == 3)
+		{
+			if(std::string(argv[2]) != "-d")
+				BOOST_LOG_TRIVIAL(warning) << "flag not recognized, ignoring flag.";
+
+		}
+
+		//set default logging level to everything above and including info
+		if(argc == 2)
+			boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::info);
 
 		NginxConfigParser parser;
 		NginxConfig out_config;
