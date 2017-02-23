4a4f840f19d4713e7f210a4047bed817343a504e
Kevin Zhu <kevinazhu@gmail.com>
2017-02-22 19:15:38 -0800

    add request_handler test for CreateByName. fix EchoHandler and NotFoundHandler tests

--- echo_handler_test.cc
+++ echo_handler_test.cc
@@ -1,25 +1,49 @@
 #include <string>
+#include <sstream>
 
 #include "gtest/gtest.h"
 #include "echo_handler.h"
+#include "request_handler.h"
+#include "response.h"
 #include "request.h"
+#include "config_parser.h"
 
-TEST(EchoHandlerTest, SimpleString) {
-	std::string raw_req = "hello";
+class EchoHandlerTest : public ::testing::Test {
+protected:
+	bool parseString(const std::string config_string) {
+		std::stringstream config_stream(config_string);
+		return parser.Parse(&config_stream, &out_config);
+	}
 
-	// generate header info from the request
-	auto request = Request::Parse(raw_req.c_str());
-	std::cout << request.raw_request() << std::endl;
+	std::string runRequest(std::string raw_req) {
+		// generate header info from the request
+		auto request = Request::Parse(raw_req.c_str());
 
-	// create an echo handler
-	EchoHandler echoHandler;
+		// create an echo handler
+		EchoHandler echoHandler;
+		if(echoHandler.Init("/echo", out_config) == RequestHandler::OK)
+		{
+			Response response;
+			echoHandler.HandleRequest(*request, &response);
+			std::string response_string = response.ToString();
+			
+			int index = response_string.find("\r\n");
+			return response_string.substr(0, index);
+		}
+		else
+		{
+			return "";
+		}
+	}
 
-	Response response;
-	std::cout << "TEST1" << std::endl;
-	echoHandler.HandleRequest(*request, &response);
-	std::cout << "TEST2" << std::endl;
-	std::string response_string = response.ToString();
+	NginxConfigParser parser;
+	NginxConfig out_config;
+};
 
-	std::string expected = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nhello";
-	EXPECT_EQ(response_string, expected);
+TEST_F(EchoHandlerTest, SimpleRequest) {
+	parseString("");
+	std::string request_string = "GET /echo HTTP/1.1\r\n\r\n";
+	std::string result = runRequest(request_string);
+
+	EXPECT_EQ(result, "HTTP/1.1 200 OK");
 }
