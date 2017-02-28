102279675d6485e73bc8225cea8bd5e12f0d7721
Richard Min <richardmin97@gmail.com>
2017-02-28 08:24:57 -0800

    HTTP_Client tests added

--- http_client_test.cc
+++ http_client_test.cc
@@ -4,3 +4,26 @@
 #include "http_client.h"
 #include "request.h"
 #include "response.h"
+
+// Note: these tests are networked in nature.
+// This means if you have internet issues, 
+// They may fail unexpectedly.
+
+TEST(HttpClientTest, EstablishConnection) {
+	HTTPClient c;
+	EXPECT_TRUE(c.EstablishConnection("www.google.com", "http"));
+    EXPECT_TRUE(c.EstablishConnection("www.google.com"));
+    EXPECT_TRUE(c.EstablishConnection("www.google.com", "https"));
+    EXPECT_FALSE(c.EstablishConnection("www....google.com", "http"));
+    EXPECT_FALSE(c.EstablishConnection("www.google.com", "f"));
+    EXPECT_FALSE(c.EstablishConnection("www.google.com", "invalid_protocol"));
+    EXPECT_FALSE(c.EstablishConnection("http://www.google.com"));
+}
+
+TEST(HttpClientTest, SendRequest) {
+    HTTPClient c;
+    auto req= Request::Parse("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n");
+    ASSERT_NEQ(req.get(), nullptr);
+    ASSERT_TRUE(c.EstablishConnection("www.google.com", "http"));
+    EXPECT_NEQ(c.SendRequest(req).get(), nullptr);
+}
