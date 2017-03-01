81fec4df8c96c5afaaa53cdc6ab98fe5e2f504c2
shijunhuang <hsjfelix@gmail.com>
2017-02-28 21:41:59 -0800

    added testing for reverse_proxy_handler

--- reverse_proxy_handler_test.cc
+++ reverse_proxy_handler_test.cc
@@ -10,74 +10,82 @@
 
 
 //Test fixture for creating configs and init RequestProxyHandler
-// class ReverseProxyHandlerTest : public ::testing::Test {
-// protected:
-// 	bool parseString(const std::string config_string) {
-// 		std::stringstream config_stream(config_string);
-// 		return parser.Parse(&config_stream, &out_config);
-// 	}
+class ReverseProxyHandlerTest : public ::testing::Test {
+protected:
+	bool parseString(const std::string config_string) {
+		std::stringstream config_stream(config_string);
+		return parser.Parse(&config_stream, &out_config);
+	}
 
-// 	std::string runRequest(std::string raw_req) {
-// 		// generate header info from the request
-// 		auto request = Request::Parse(raw_req.c_str());
+	RequestHandler::Status initProxy(std::string prefix){
+		return proxy_handler.Init(prefix,out_config);
+	}
 
-// 		// create an proxy handler
-// 		StaticHandler staticHandler;
-// 		if(staticHandler.Init("/proxy", out_config) == RequestHandler::OK)
-// 		{
-// 			Response response;
-// 			staticHandler.HandleRequest(*request, &response);
-// 			std::string response_string = response.ToString();
-			
-// 			int index = response_string.find("\r\n");
-// 			return response_string.substr(0, index);
-// 		}
-// 		else
-// 		{
-// 			return "";
-// 		}
-// 	}
+	std::string getPrefix(){
+		return proxy_handler.prefix_;
+	}
 
-// 	NginxConfigParser parser;
-// 	NginxConfig out_config;
-// };
+	std::string getProtocol(){
+		return proxy_handler.protocol_;
+	}
 
-// TEST_F(StaticHandlerTest, SimpleTest) {
-// 	parseString("proxy_pass www.ucla.edu/;");
-// 	std::string request_string = "GET /proxy/ HTTP/1.1\r\n\r\n";
-// 	std::string result = runRequest(request_string);
-	
-// 	EXPECT_EQ(result, "HTTP/1.1 200 OK");
-// }
+	std::string getHost(){
+		return proxy_handler.host_;
+	}
 
-// TEST_F(StaticHandlerTest, Simple404Test) {
-// 	parseString("proxy_pass www.ucla.edu/;");
-// 	std::string request_string = "GET /static/error.jpg HTTP/1.1\r\n\r\n";
-// 	std::string result = runRequest(request_string);
+	std::string getPath(){
+		return proxy_handler.path_;
+	}
 
-// 	EXPECT_EQ(result, "HTTP/1.1 404 Not Found");
-// }
+	std::string getUrl(){
+		return proxy_handler.url_;
+	}
 
-// TEST_F(StaticHandlerTest, UnknownFileTest) {
-// 	parseString("root static/;");
-// 	std::string request_string = "GET /static/bad HTTP/1.1\r\n\r\n";
-// 	std::string result = runRequest(request_string);
+	std::unique_ptr<Response> VisitOutsideServer(const Request& request, std::string host, std::string service) const{
+		return proxy_handler.VisitOutsideServer(request,host,service);
+	}
 
-// 	EXPECT_EQ(result, "HTTP/1.1 404 Not Found");
-// }
+	Request TransformIncomingRequest(const Request& request) const{
+		return proxy_handler.TransformIncomingRequest(request);
+	}
 
-// TEST_F(StaticHandlerTest, UnsupportedTest) {
-// 	parseString("root static/;");
-// 	std::string request_string = "GET /static/static_handler.o HTTP/1.1\r\n\r\n";
-// 	std::string result = runRequest(request_string);
+	NginxConfigParser parser;
+	NginxConfig out_config;
+	ReverseProxyHandler proxy_handler;
+};
 
-// 	EXPECT_EQ(result, "HTTP/1.1 404 Not Found");
-// }
+TEST_F(ReverseProxyHandlerTest, Init) {
+	parseString("proxy_pass http://www.ucla.edu/static;");
+	auto init_status = initProxy("/proxy");
+	EXPECT_EQ(init_status,RequestHandler::OK);
+	EXPECT_EQ(getPrefix(), "/proxy");
+	EXPECT_EQ(getProtocol(), "http");
+	EXPECT_EQ(getHost(), "www.ucla.edu/static");
+	EXPECT_EQ(getPath(), "/static");
+	EXPECT_EQ(getUrl() , "http://www.ucla.edu/static");
+}
 
-// TEST_F(StaticHandlerTest, BadInit) {
-// 	parseString("");
-// 	std::string request_string = "GET /static/kinkakuji.jpg HTTP/1.1\r\n\r\n";
-// 	std::string result = runRequest(request_string);
+TEST_F(ReverseProxyHandlerTest, IllegalConfig) {
+	parseString("proxy_ http://www.ucla.edu;");
+	auto init_status = initProxy("/proxy");
+	EXPECT_EQ(init_status,RequestHandler::ERROR);
 
-// 	EXPECT_EQ(result, "");
-// }
+	parseString("proxy_pass;");
+	init_status = initProxy("/proxy");
+	EXPECT_EQ(init_status,RequestHandler::ERROR);
+}
+
+TEST_F(ReverseProxyHandlerTest, NoProtocol) {
+	parseString("proxy_pass www.ucla.edu;");
+	auto init_status = initProxy("/proxy");
+	EXPECT_EQ(init_status,RequestHandler::ERROR);
+}
+
+TEST_F(ReverseProxyHandlerTest, TransformRequest){
+	parseString("proxy_pass www.ucla.edu/static;");
+	initProxy("/proxy");
+	auto req = Request::Parse("GET /proxy/yo.txt HTTP/1.1\r\n\r\n");
+	std::string new_req = TransformIncomingRequest(*req).ToString();
+	EXPECT_EQ("", new_req);
+
+}
