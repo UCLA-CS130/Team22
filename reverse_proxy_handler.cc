e2d91ef4e1597e399b40f6be341a94d1994c3b47
Richard Min <richardmin97@gmail.com>
2017-02-28 21:34:13 -0800

    Yet another bug fix

--- reverse_proxy_handler.cc
+++ reverse_proxy_handler.cc
@@ -33,7 +33,7 @@ RequestHandler::Status ReverseProxyHandler::Init(const std::string& uri_prefix, 
 			}
 			else {
 				host_ = url_.substr(protocol_pos + 2);
-				path_ = "";
+				path_ = "/";
 			}
 			return RequestHandler::OK;
 		}
@@ -48,7 +48,7 @@ RequestHandler::Status ReverseProxyHandler::HandleRequest(const Request& request
 	BOOST_LOG_TRIVIAL(trace) << "Creating Reverse Proxy Response...";
 
 	Request OutgoingRequest = TransformIncomingRequest(request);
-	BOOST_LOG_TRIVIAL(trace) << "Reaching out to " << OutgoingRequest.uri();
+	BOOST_LOG_TRIVIAL(trace) << "Reaching out to " << OutgoingRequest.headers()[0].second << " with URI: " << OutgoingRequest.uri();
 	// TODO: Send the outgoing request
 	// for(int i = 0; i < MaxRedirectDepth + 1; i++) {
 	// BOOST_LOG_TRIVIAL(trace) << "Reaching out to " << OutgoingRequest.uri();
